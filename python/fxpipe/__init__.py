79f73810cefdffe0d1891b2dafbe88eaaeffce56
Robert Nederhorst <throb@throb.net>
2012-04-18 08:29:39 -0700

    added fxpipe module

---
+++ python/fxpipe/__init__.py
@@ -0,0 +1,40 @@
+import platform
+
+'''
+Set the variables here how you want the paths to work
+
+Set the paths for windows, osx, linux as you see fit here.
+
+'''
+
+win_jobpath = 'z:/job'
+osx_jobpath = '/Volumes/job'
+lin_jobpath = '/mnt/job'
+
+### DO NOT CHANGE BELOW THIS LINE ###
+
+### make sure we set the right path based on OS type
+if platform.system() == 'Windows':
+    jobPath = win_jobpath
+if platform.system() == 'Linux':
+    jobPath = lin_jobpath
+if platform.system() == 'Darwin':
+    jobPath = osx_jobpath
+
+def fixPath(inputPath):
+    '''
+    This function is what is used to transpose paths from one OS to another should these paths exist
+    returns the fixed path based on OS type
+    '''
+    if platform.system() == 'Windows':
+        newPath = inputPath.replace (lin_jobpath, win_jobpath)
+        newPath = inputPath.replace (osx_jobpath, win_jobpath)
+        return newPath
+    if platform.system() == 'Linux':
+        newPath = inputPath.replace (win_jobpath, lin_jobpath)
+        newPath = inputPath.replace (osx_jobpath, lin_jobpath)
+        return newPath
+    if platform.system() == 'Darwin':
+        newPath = inputPath.replace (lin_jobpath, osx_jobpath)
+        newPath = inputPath.replace (win_jobpath, osx_jobpath)
+        return newPath
