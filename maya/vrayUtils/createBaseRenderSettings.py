8a6c92bb339fb52f54d0d69f3e4c98649108d07d
Robert Nederhorst <throb@throb.net>
2012-05-10 22:24:44 -0700

    [bug] move line that created possible error : fixed

--- maya/vrayUtils/createBaseRenderSettings.py
+++ maya/vrayUtils/createBaseRenderSettings.py
@@ -1,17 +1,20 @@
 import maya.cmds as cmds
+import maya.utils as utils
+import maya.mel as mel
 
 def createBaseRenderSettings(job='', seq='', shot=''):
     '''
     Set up some basic render settings that get reasonable speed and quality along with consistency
     Uses the dmc sampler and gauss filtering
     '''
+
     cmds.setAttr('vraySettings.sys_low_thread_priority', 1)
+    cmds.setAttr('vraySettings.dontSaveImage', 1)
     cmds.setAttr('vraySettings.imageFormatStr', 'exr (multichannel)', type='string')
     cmds.setAttr('vraySettings.imgOpt_exr_compression', 3)
     cmds.setAttr('vraySettings.imgOpt_exr_bitsPerChannel', 16)
     cmds.setAttr('vraySettings.imgOpt_exr_autoDataWindow', 1)
-    exrAttrs = ("job='%s';seq='%s';shot='%s'") % (job, seq, shot)
-    cmds.setAttr('vraySettings.imgOpt_exr_attributes', exrAttrs, type='string')
+
     cmds.setAttr('vraySettings.sRGBOn', 1)
     cmds.setAttr('vraySettings.vfbOn', 1)
     cmds.setAttr('vraySettings.aaFilterType', 6)
@@ -20,4 +23,12 @@ def createBaseRenderSettings(job='', seq='', shot=''):
     cmds.setAttr('vraySettings.cmap_adaptationOnly', 1)
     cmds.setAttr('vraySettings.dmcs_adaptiveAmount', 0.9)
     cmds.setAttr('vraySettings.sys_low_thread_priority', 1)
-    cmds.setAttr('vraySettings.sys_rayc_dynMemLimit', 8000)
+    cmds.setAttr('vraySettings.sys_rayc_dynMemLimit', 8000) 
+    exrAttrs = ("job='%s';seq='%s';shot='%s'") % (job, seq, shot)
+    cmds.setAttr('vraySettings.imgOpt_exr_attributes', exrAttrs, type='string')
+        
+        
+        
+
+        
+      
