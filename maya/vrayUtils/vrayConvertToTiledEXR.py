63b533ccd9dca68888a23955a1f9b9900d7a0a6d
throb <user@ws001.(none)>
2012-07-12 11:09:16 -0700

    added check for the tiled exr maker for debugging purposes.

--- maya/vrayUtils/vrayConvertToTiledEXR.py
+++ maya/vrayUtils/vrayConvertToTiledEXR.py
@@ -1,5 +1,7 @@
 import maya.cmds as cmds
 import maya.mel as mel
+import sys
+import platform
 
 def vrayConvertToTiledEXR(): 
     '''
@@ -12,13 +14,21 @@ def vrayConvertToTiledEXR():
     mayaVersion = re.search('[0-9]+',re.search('maya[0-9]+',sys.argv[0].lower()).group(0)).group(0) # look for the 2012 or 2013 etc in path name
     pathToVRayTools = os.path.normpath(os.environ['VRAY_TOOLS_MAYA%s_x64' % (mayaVersion)]) # make sure we have the right vray tools path
     allFileNodes = cmds.ls(type='file') # look for file nodes
+    if platform.system() == 'Windows':
+        execFile = '"%s/img2tiledexr.exe"' % (pathToVRayTools) # needs quotes in case there are spaces in filename
+    else:
+        execFile = '"%s/img2tiledexr"' % (pathToVRayTools) # needs quotes in case there are spaces in filename
+    if os.path.exists(os.path.normpath(execFile.replace('"',''))) == False:    
+        print '%s does not exist.  exiting.' % (execFile)
+        sys.exit(1)
     nonFloatFiles = []
     for curFile in allFileNodes:
         curFileName = cmds.getAttr(curFile + '.fileTextureName')
         if 'exr' not in os.path.splitext(curFileName)[1].lower(): #check for exr
             print 'Converting %s' % (curFileName)
             newFileName = '%s_tiled.exr' % ( os.path.splitext(curFileName)[0] )
-            execFile = '"%s/img2tiledexr"' % (pathToVRayTools) # needs quotes in case there are spaces in filename
+            
+                                
             subprocess.call(execFile + ' ' + curFileName + ' ' + newFileName, shell=True) #actually do the work
             if os.path.exists(newFileName):
                 cmds.setAttr(curFile + '.fileTextureName', newFileName, type='string') # replace old filename with a new one
