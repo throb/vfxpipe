d773f0b3567cbe304224c5ece1415e7f54d006d6
unknown <robn@RENTWRK02.a52.com>
2014-06-26 18:12:36 -0700

    [FEATURE] Add support for Nuke multiple job paths

--- nuke/filenameFilters.py
+++ nuke/filenameFilters.py
@@ -6,20 +6,30 @@ import nuke
 import fxpipe
 
 def fxPipeFileNameFilter(fileName):
+    swapped = False
     if nuke.env['LINUX']:
-        fileName = pathSwap(fileName, fxpipe.jobPathWin, fxpipe.jobPathLin)
-        fileName = pathSwap(fileName, fxpipe.jobPathOsx, fxpipe.jobPathLin)
+        fileName, swapped = pathSwap(fileName, fxpipe.jobPathWin, fxpipe.jobPathLin)
+        if swapped == False:
+            fileName = pathSwap(fileName, fxpipe.jobPathOsx, fxpipe.jobPathLin)
     if nuke.env['MACOS']:
-        fileName = pathSwap(fileName, fxpipe.jobPathLin, fxpipe.jobPathOsx)
-        fileName = pathSwap(fileName, fxpipe.jobPathWin, fxpipe.jobPathOsx)        
+        fileName, swapped = pathSwap(fileName, fxpipe.jobPathLin, fxpipe.jobPathOsx)
+        if swapped == False:
+            fileName, swapped = pathSwap(fileName, fxpipe.jobPathWin, fxpipe.jobPathOsx)        
     if nuke.env['WIN32']:
-        fileName = pathSwap(fileName, fxpipe.jobPathLin, fxpipe.jobPathWin)
-        fileName = pathSwap(fileName, fxpipe.jobPathOsx, fxpipe.jobPathWin)
+        fileName, swapped = pathSwap(fileName, fxpipe.jobPathLin, fxpipe.jobPathWin)
+        if swapped == False:
+            fileName, swapped = pathSwap(fileName, fxpipe.jobPathOsx, fxpipe.jobPathWin)
     return fileName
 
 def pathSwap(inFile, pathA, pathB):
+    swapped = False
     for pathCount in range(min(len(pathA),len(pathB))):
-        fileName = inFile.replace(pathA[pathCount],pathB[pathCount])
-    return fileName
+        if swapped == False:
+            fileName = inFile.replace(pathA[pathCount],pathB[pathCount])
+            if fileName != inFile:
+                swapped = True
+                continue
+        print pathCount, pathA[pathCount], pathB[pathCount], swapped
+    return fileName, swapped
 
 nuke.addFilenameFilter(fxPipeFileNameFilter)
