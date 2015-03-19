19191a2dd67bef9cf6f214c362911b6f9b5ad0bd
Robert Nederhorst <throb@throb.net>
2015-03-18 19:46:11 -0700

    [FEATURE] Added a progress bar to exr converter

--- maya/vrayUtils/vrayConvertToTiledEXR.py
+++ maya/vrayUtils/vrayConvertToTiledEXR.py
@@ -2,6 +2,8 @@ import maya.cmds as cmds
 import maya.mel as mel
 import sys
 import platform
+import os, sys, subprocess, platform, re
+import pymel.core as pm
 
 def vrayConvertToTiledEXR(): 
     '''
@@ -9,11 +11,14 @@ def vrayConvertToTiledEXR():
 
     '''
 
-    import os, sys, subprocess, platform, re
+    # setup a window
+    window = pm.window()
+    pm.columnLayout()
     
     mayaVersion = re.search('[0-9]+',re.search('maya[0-9]+',sys.argv[0].lower()).group(0)).group(0) # look for the 2012 or 2013 etc in path name
     pathToVRayTools = os.path.normpath(os.environ['VRAY_TOOLS_MAYA%s_x64' % (mayaVersion)]) # make sure we have the right vray tools path
     allFileNodes = cmds.ls(type='file') # look for file nodes
+    progressControl = pm.progressBar(maxValue=len(allFileNodes)-1, width=300) # make sure window is prepped for size
     if platform.system() == 'Windows':
         execFile = '"%s/img2tiledexr.exe"' % (pathToVRayTools) # needs quotes in case there are spaces in filename
     else:
@@ -22,7 +27,9 @@ def vrayConvertToTiledEXR():
         print '%s does not exist.  exiting.' % (execFile)
         sys.exit(1)
     nonFloatFiles = []
+    pm.showWindow( window )
     for curFile in allFileNodes:
+        pm.progressBar(progressControl,edit=True,step=1)
         curFileName = cmds.getAttr(curFile + '.fileTextureName')
         if 'exr' not in os.path.splitext(curFileName)[1].lower(): #check for exr
             print 'Converting %s' % (curFileName)
